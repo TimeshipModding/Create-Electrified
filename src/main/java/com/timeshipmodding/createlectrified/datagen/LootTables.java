ae05643780236331e9920163d83075c50ad6ac20
TardisModding <doctorwho5611@gmail.com>
2023-04-03 14:07:40 +1000

    Bug Fixing and Compiling 1.19.2pre-v0.1

--- src/main/java/com/timeshipmodding/createlectrified/datagen/LootTables.java
+++ src/main/java/com/timeshipmodding/createlectrified/datagen/LootTables.java
@@ -2,6 +2,7 @@ package com.timeshipmodding.createlectrified.datagen;
 
 import com.timeshipmodding.createlectrified.providers.BaseLootTableProvider;
 import com.timeshipmodding.createlectrified.registries.ModBlocks;
+import com.timeshipmodding.createlectrified.registries.ModCasingBlocks;
 import com.timeshipmodding.createlectrified.registries.ModItems;
 import net.minecraft.data.DataGenerator;
 
@@ -20,5 +21,6 @@ public class LootTables extends BaseLootTableProvider {
         lootTables.put(ModBlocks.RAW_ALUMINIUM_BLOCK.get(), createSimpleTable("raw_aluminium_block", ModBlocks.RAW_ALUMINIUM_BLOCK.get()));
         lootTables.put(ModBlocks.ALUMINIUM_BLOCK.get(), createSimpleTable("aluminium_block", ModBlocks.ALUMINIUM_BLOCK.get()));
         lootTables.put(ModBlocks.ELECTRUM_BLOCK.get(), createSimpleTable("electrum_block", ModBlocks.ELECTRUM_BLOCK.get()));
+        lootTables.put(ModCasingBlocks.ALUMINIUM_CASING.get(), createSimpleTable("aluminium_casing", ModCasingBlocks.ALUMINIUM_CASING.get()));
     }
 }
